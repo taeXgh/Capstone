8779c7d8a311727f89cd15a8276a12232891d696
taeXgh <talopalo10@gmail.com>
2026-03-25 19:06:27 -0400

    Update .gitignore and refactor ReadWriteGoogleSheets.py for improved file handling

--- pythonProject/readGoogleSheets/ReadWriteGoogleSheets.py
+++ pythonProject/readGoogleSheets/ReadWriteGoogleSheets.py
@@ -8,65 +8,76 @@
 # Author: Thalia Edwards
 #
 # Date: 03/02/2026
+#
+# Edits by: Taha Chowdhury between 3-02-26 - 3-12-2026
 #------------------------------------------------------------------------
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from docx import Document
+from pathlib import Path
+
+
+#username = "talop"
+
+def get_resume_link(resume):
+    return resume.split('"')[1]
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('pythonProject/secret_key/secret_key.json', scopes=scopes)
+SCRIPT_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = SCRIPT_DIR.parent   # pythonProject
+
+key_path = PROJECT_DIR / "secret_key" / "secret_key.json"
+template_path = PROJECT_DIR / "Applications" / "Application Template - Copy.docx"
+
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(key_path, scopes=scopes)
 
 file = gspread.authorize(creds)
 workbook = file.open('application-form-responses')
 sheet = workbook.sheet1
 
-#print every cell in the range A2:R502
-# for cell in sheet.range('A2:R4'):
-#     if cell.value != '':
-#         print(cell.value)
-#     else:
-#         print('No Value')
-#desired output:
-# Submission Date: 02/18/2026 12:00:00
-# Name: John Doe
-# Email: tedwar6@gmu.edu
-# Subject: test
-# Position: Certified Nurse Assistant
-# File Upload: resume.docx
-# are you currently employed: 	Yes
-# do you have a nursing license: 	No
-# have you ever worked with us before:	No
-# how did you hear about us:	Online
-# if yes why did you leave:	Empty
-# if yes with who:	George Mason University
-# work availability:	Part Time
-# work phone:	(555) 555-5555
-# authorization:	Yes
-# preferred method of contact:	Email
 
-# Get headers from row 1 (A1:R1)
-headers = [h.strip() for h in sheet.row_values(1)]
-
-# Get response rows (A2:R4)
-rows = sheet.get_all_values()[1:4]   # rows 2-4
-
-for row in rows:
-    # Pad short rows so zip doesn't stop early
-    padded_row = row + [''] * (len(headers) - len(row))
-
-    for header, value in zip(headers, padded_row):
-        clean_value = value.strip()
-        display_value = clean_value if clean_value else "Empty"
-        #print(f"{header}: {display_value}")   # f-string
-        print("{:<38} {}".format(f"{header}:", display_value)) #.format for alignment
-    print()  # blank line between submissions
+rows = sheet.get_all_values(value_render_option = "FORMULA") 
 
 
-#print every cell in the range A:A
-#print(sheet.range('A:A'))
+for row in rows[1:]:
+    application = {
+    "submission":         row[0],
+    "name":               row[1],
+    "email":              row[2],
+    "position":           row[3],
+    "resume_URL":         get_resume_link(str(row[4])),
+    "employment_status":  row[5],
+    "prior" :             row[6],
+    "hear_about" :        row[7],
+    "reason_left" :       row[8],
+    "current_employer" :  row[9],
+    "availability" :      row[10],
+    "phone" :             row[11],
+    "preferred" :         row[12],
+    "acknowledgement" :   row[13],
+    "age":                row[14],
+    }
 
-#write to a cell
-#sheet.update_acell('B2', 'Hello World')
+    print(application)
+    print()
+
+    applicant = application.get("name")
+
+    output_path = PROJECT_DIR / "Applications" / f"{applicant} Application.docx"
+    
+    document = Document(template_path)
+    for key, value in application.items():
+        for p in document.paragraphs:
+            if key == "resume_URL":
+                if f"[{key}]" in p.text:
+                    p.text = p.text.replace(f"[{key}]", str(value))
+
+            elif f"[{key}]" in p.text:
+                p.text = p.text.replace(f"[{key}]", str(value))
+                
+    document.save(output_path)
